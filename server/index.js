c0b7c90eb7bfc5fe2cb8752efc733291a8072454
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-09-30 16:24:02 +0530

    30 sep commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
+const bodyParser = require('bodyParser')
+const cookieParser = require('cookieParser');
+
 
 mongoose.connect('mongodb+srv://srUser:password@srUser@searchrate.vumee.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
                     {useNewUrlParser: true}).then(()=>console.log('Database connected')).catch(er => console.log(er))
