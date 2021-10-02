f67b6414ff37ca8c9c1bb8d85c3247e9c30a8cfa
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-10-02 17:24:55 +0530

    1 oct commit

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
-const bodyParser = require('bodyParser')
-const cookieParser = require('cookieParser');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
+const {User} = require('./models/user')
 
 
-mongoose.connect('mongodb+srv://srUser:password@srUser@searchrate.vumee.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-                    {useNewUrlParser: true}).then(()=>console.log('Database connected')).catch(er => console.log(er))
+mongoose.connect('mongodb+srv://srUser:password@srUser@searchrate.vumee.mongodb.net/SRdb?retryWrites=true&w=majority',
+                    { useNewUrlParser: true, useUnifiedTopology: true}).then(()=>console.log('Database connected')).catch(er => console.log(er))
+
+
+app.use(bodyParser.urlencoded({ extended:true }))
+app.use(bodyParser.json())
+app.use(cookieParser());
+
+app.post('/api/users/register',(req,res)=>{
+   const user = User(req.body);
+   user.save((err, user)=>{
+       if(err) return res.json({success:false,err})
+    return res.status(200).json({success:true});
+   })
+})
 
 app.get('/', (req,res)=>{
-    console.log('on!')
+    console.log('server on!')
     res.send('started!')
 })
 
