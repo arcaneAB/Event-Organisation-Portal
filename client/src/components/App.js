0c7567595581245b35b5150fbd72a025d09295b8
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-10-12 20:00:10 +0530

    Start client side development

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+          <Route path="/" component={Home}/>
+          <Route path="/about" component={About} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
