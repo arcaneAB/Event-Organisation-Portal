b549e1e2feedfdbf53a6871a786b14397bcf50ec
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-11-08 13:25:15 +0530

    Landing page start

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,5 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+export const API_KEY = "b09a7dc72ddc09c6d398bb701b4e41b0";
+export const API_URL = "https://api.themoviedb.org/3/";
+export const IMAGE_URL = "http://image.tmdb.org/t/p/";
